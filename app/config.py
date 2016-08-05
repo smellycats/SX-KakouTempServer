0e9b85b5945a6fe01997ffa924f6f89973ceb8de
SmellyCat <smellycat2014@foxmail.com>
2016-08-05 11:32:57 +0800

    添加  基础文件

---
+++ app/config.py
@@ -0,0 +1,48 @@
+﻿# -*- coding: utf-8 -*-
+
+
+class Config(object):
+    # 密码 string
+    SECRET_KEY = 'hellokitty'
+    # 服务器名称
+    HEADER_SERVER = 'SX-KakouTempServer'
+    # 加密次数 int
+    ROUNDS = 123456
+    # token生存周期，默认2小时 int
+    EXPIRES = 7200
+    # 数据库连接 string
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../kakou.db'
+    # 数据库连接绑定 dict
+    SQLALCHEMY_BINDS = {
+        'kakou': 'mysql://kakou:kakou@10.47.222.50/kakou_ser'
+    }
+    # 连接池大小 int
+    #SQLALCHEMY_POOL_SIZE = 5
+    # 用户权限范围 dict
+    SCOPE_USER = {}
+    # 白名单启用 bool
+    WHITE_LIST_OPEN = True
+    # 白名单列表 set
+    WHITE_LIST = set()
+    
+    FLAG_IP = {
+	1: '10.47.223.152',
+	2: '10.47.223.150',
+	3: '10.47.223.149',
+	4: '10.47.223.148',
+	5: '10.47.223.146',
+	6: '10.47.223.145'
+    }
+
+
+
+class Develop(Config):
+    DEBUG = True
+
+
+class Production(Config):
+    DEBUG = False
+
+
+class Testing(Config):
+    TESTING = True
---
+++ app/helper_kakou.py
@@ -0,0 +1,49 @@
+﻿# -*- coding: utf-8 -*-
+import json
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class Kakou(object):
+    def __init__(self, **kwargs):
+        self.host = kwargs['host']
+        self.port = kwargs['port']
+	self.username = kwargs['username']
+	self.password = kwargs['password']
+        self.headers = {'content-type': 'application/json'}
+
+    def get_kakou_maxid(self):
+        """根据时间,地点,方向获取车流量"""
+        url = 'http://{0}:{1}/maxid'.format(self.host, self.port)
+        #print url
+        try:
+            r = requests.get(url, headers=self.headers,
+			     auth=HTTPBasicAuth(self.username, self.password))
+            if r.status_code == 200:
+                return json.loads(r.text)
+        except Exception as e:
+            raise
+
+    def get_kakou_info(self, start_id, end_id):
+        """根据id范围获取车辆信息"""
+        url = 'http://{0}:{1}/kakou/{2}/{3}'.format(
+            self.host, self.port, start_id, end_id)
+        try:
+            r = requests.get(url, headers=self.headers,
+			     auth=HTTPBasicAuth(self.username, self.password))
+            if r.status_code == 200:
+                return json.loads(r.text)
+        except Exception as e:
+            raise
+
+    def get_kkdd_all(self):
+        """获取所有卡口地点名称"""
+        url = 'http://{0}:{1}/kkdd'.format(self.host, self.port)
+        try:
+            r = requests.get(url, headers=self.headers,
+			     auth=HTTPBasicAuth(self.username, self.password))
+            if r.status_code == 200:
+                return json.loads(r.text)
+        except Exception as e:
+            raise
